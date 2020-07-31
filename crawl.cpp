b161abb6b4b19a69ad52e5794051f82a4c0c85aa
Jim Stallings <stallingsjm@mymail.vcu.edu>
2020-07-31 17:50:57 -0400

    Update crawl.cpp

--- crawl.cpp
+++ crawl.cpp
@@ -6,11 +6,11 @@ int main()
     int two_d[3][4] = {{0,1,2,3}, {4,5,6,7}, {8,9,10,11}};
     int rows = 3;
     int cols = 4;
-    int rowsCols = rows*cols;
+    int rowsCols = rows * cols;
     for (int i = 0; i < rowsCols; ++i)
     {
-        auto& inner = two_d[i/rowsCols];
-        cout << inner[i%rowsCols]<< endl;
+        auto &inner = two_d[i / rowsCols];
+        cout << inner[i % rowsCols]<< endl;
     }
     cout << "\n Yaay, it worked!\n" << endl;
     return 0;
